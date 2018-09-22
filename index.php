170a21c5fe68e4f82aaad8f484f5f771b7693d71
unknown <johnathan.monsalve@udea.edu.co>
2018-09-22 11:38:18 -0500

    Fix

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
 </head>
 <body>
     <?php 
-    $name = $_GET['name'];
-    echo '<p>Hola Mundo $name Heroku</p>'; 
+    //$name = $_GET['name'];
+    $name = "Jm";
+    echo '<p>Hola Mundo .$name. Heroku</p>'; 
     ?>
 </body>
 </html>
